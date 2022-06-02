c30e80dcb94b8f5f14bc1998ebb1d326ada085e3
thiago <taigomesquita.tm@gmail.com>
2022-06-02 10:48:06 -0300

    first commit

---
+++ scr/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+
+
+
+function  MeuComponente(){
+  return <div>Treinarweb</div>
+}
+
+const Home: NextPage = () => {
+  return (
+    <div>
+     <MeuComponente/>
+    </div>
+  )
+}
+
+export default Home
---
+++ scr/ui/components/cabecalho.style.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@mui/system";
+
+export const CabecalhoContainer = styled('header') `
+    color: red;
+`;
+
+<CabecalhoContainer/>
+
+
+export const Logo = styled ('img')``;
